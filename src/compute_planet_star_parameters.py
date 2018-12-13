dd1a336537b97072c50b2ba926f19306e925d689
lgbouma <bouma.luke@gmail.com>
2018-12-13 11:18:09 -0500

    update compute_planet_star_parameters to use TESS lc

--- src/compute_planet_star_parameters.py
+++ src/compute_planet_star_parameters.py
@@ -4,15 +4,82 @@ estimate the stellar density, and mass.
 """
 from __future__ import division, print_function
 import numpy as np
+import pickle
 
 from astropy import units as u, constants as const
 
-# use parameters from Table 4 of Hoyer+2013
-i = 88.52*u.deg
-a_by_Rs = 5.463
-a_by_Rs_perr = 0.025
-a_by_Rs_merr = 0.020
-P = 1.33823204*u.day
+# Keivan value
+Rstar = 0.893*u.Rsun
+Rstar_err = 0.024*u.Rsun
+
+# parameters from Table 4 of Hoyer+2013, for comparison
+i_hoyer13 = 88.52*u.deg
+a_by_Rs_hoyer13 = 5.463
+a_by_Rs_perr_hoyer13 = 0.025
+a_by_Rs_merr_hoyer13 = 0.020
+P_hoyer13 = 1.33823204*u.day
+hoyer_params = [i_hoyer13, a_by_Rs_hoyer13, a_by_Rs_perr_hoyer13,
+                a_by_Rs_merr_hoyer13, P_hoyer13]
+
+# get parameters from fit to phase-folded lightcurve
+picklepath = ('/home/luke/Dropbox/proj/tessorbitaldecay/results/'
+              'tess_lightcurve_fit_parameters/402026209/'
+              '402026209_phased_mandelagol_fit_empiricalerrs.pickle' )
+
+d = pickle.load(open(picklepath, 'rb'))
+i = d['fitinfo']['finalparams']['incl']*u.deg
+i_upper = i + 0.39*u.deg
+
+i = 88.52*u.deg # Hoyer+ 2013, table 4
+i_upper = i + d['fitinfo']['finalparamerrs']['std_perrs']['incl']*u.deg
+i_lower = i - d['fitinfo']['finalparamerrs']['std_merrs']['incl']*u.deg
+a_by_Rs = d['fitinfo']['finalparams']['sma']
+a_by_Rs_perr = d['fitinfo']['finalparamerrs']['std_perrs']['sma']
+a_by_Rs_merr = d['fitinfo']['finalparamerrs']['std_merrs']['sma']
+P = d['fitinfo']['finalparams']['period']*u.day
+
+a_by_Rs = d['fitinfo']['finalparams']['sma']
+a_by_Rs_perr = d['fitinfo']['finalparamerrs']['std_perrs']['sma']
+a_by_Rs_merr = d['fitinfo']['finalparamerrs']['std_merrs']['sma']
+
+Rp_by_Rs = d['fitinfo']['finalparams']['rp']
+Rp_by_Rs_err = np.mean(
+    [d['fitinfo']['finalparamerrs']['std_perrs']['rp'],
+    d['fitinfo']['finalparamerrs']['std_merrs']['rp']
+    ]
+)
+Rp = Rp_by_Rs * Rstar
+Rp_upper = (Rp_by_Rs + Rp_by_Rs_err)*(Rstar+Rstar_err)
+Rp_lower = (Rp_by_Rs - Rp_by_Rs_err)*(Rstar-Rstar_err)
+
+my_params = [i, a_by_Rs, a_by_Rs_perr, a_by_Rs_merr, P]
+
+paramnames = ['incl','a/Rstar','a/Rstar_perr','a/Rstar_merr','P']
+
+# the period you adopt here does not particularly matter! the one reported in
+# the transit time table does. so using my less precise TESS-only BLS period
+# (1.33814, vs hoyer+13: 1.338232 d, is fine.
+
+print('-'*42)
+for hparam, mparam, pname in zip(hoyer_params, my_params, paramnames):
+    print('{:s}: me: {:.5f}, hoyer+13: {:5f}\n\tme-h13: {:.5f}'.
+          format(pname, mparam, hparam, mparam-hparam))
+print('-'*42)
+print('for selected system parameters table:')
+# Rp/Rstar, incl, a/Rstar, ulinear, uquad:
+params = ['rp','incl','sma','u_linear','u_quad']
+precisions = [5, 2, 3, 3, 3]
+for param, precision in zip(params, precisions):
+    value = d['fitinfo']['finalparams'][param]
+    value_perr = d['fitinfo']['finalparamerrs']['std_perrs'][param]
+    value_merr = d['fitinfo']['finalparamerrs']['std_merrs'][param]
+    outstr = ('{:s}: '.format(param)+
+              '{1:.{0}f}'.format(precision, value)+
+              ' +{1:.{0}f}'.format(precision, value_perr)+
+              ' -{1:.{0}f}'.format(precision, value_merr)
+    )
+    print(outstr)
+print('-'*42)
 
 # eq 30 of Winn (2010).
 # note k^3 = 3e-3, and the density of jupiter is about equal to the density of
@@ -35,10 +102,6 @@ print(
     )
 )
 
-# Keivan value
-Rstar = 0.893*u.Rsun
-Rstar_err = 0.024*u.Rsun
-
 Mstar = (4*np.pi)/3*rho_star*Rstar**3
 Mstar_lower = (4*np.pi)/3*rho_star_lower*(Rstar-Rstar_err)**3
 Mstar_upper = (4*np.pi)/3*rho_star_upper*(Rstar+Rstar_err)**3
@@ -53,12 +116,6 @@ print(
 
 ##########################################
 # now get R_p
-
-Rp_by_Rs = 0.1538   #measured by me, from TESS
-Rp_by_Rs_err = 0.0013
-Rp = Rp_by_Rs * Rstar
-Rp_upper = (Rp_by_Rs + Rp_by_Rs_err)*(Rstar+Rstar_err)
-Rp_lower = (Rp_by_Rs - Rp_by_Rs_err)*(Rstar-Rstar_err)
 
 print(
     'Rp = {:.3f} +({:.3f}) -({:.3f}) Rjup'.format(
@@ -87,9 +144,6 @@ K = 241.1 * u.m/u.s
 K_perr = 2.8 * u.m/u.s
 K_merr = 3.1 * u.m/u.s
 
-i = 88.52*u.deg # Hoyer+ 2013, table 4
-i_upper = i + 0.39*u.deg
-i_lower = i - 0.26*u.deg
 sini = float(np.sin(i))
 sini_upper = float(np.sin(i_upper))
 sini_lower = float(np.sin(i_lower))
@@ -136,7 +190,6 @@ print(
     )
 )
 
-#TODO: add errs
 plx = 3.7145e-3 * u.arcsec
 plx_offset = 82e-6 * u.arcsec
 plx += plx_offset
