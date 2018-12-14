6710862dabeb9fa3386d19c0ffe406b5a43ca8b2
lgbouma <lgbouma@mit.edu>
2018-12-13 20:02:05 -0500

    update clock offset statistics

--- src/percent_to_sigma_converter.py
+++ src/percent_to_sigma_converter.py
@@ -26,18 +26,22 @@ def get_sigma_given_percent(percent):
 
 if __name__=="__main__":
 
-    print('WASP-6b, t_systematic_offset < -77.68 sec ruled out at XX sigma')
-    frac = 1 - 0.000000439560
+    print('WASP-6b, t_systematic_offset < -81.50 sec ruled out at XX sigma')
+    val_6b = 0.000000439560
+    frac = 1 - val_6b
     get_sigma_given_percent(frac)
 
-    print('WASP-46b, t_systematic_offset < -77.68 sec ruled out at XX sigma')
-    frac = 1 - 0.026633342800
+    print('WASP-46b, t_systematic_offset < -81.50 sec ruled out at XX sigma')
+    val_46b = 0.022780460097
+    frac = 1 - val_46b
     get_sigma_given_percent(frac)
 
-    print('WASP-18b, t_systematic_offset < -77.68 sec ruled out at XX sigma')
-    frac = 1- 0.015987957966
+    print('WASP-18b, t_systematic_offset < -81.50 sec ruled out at XX sigma')
+    val_18b = 0.012510650383
+    frac = 1- val_18b
     get_sigma_given_percent(frac)
 
-    print('multiply 6b,18b, 46b: t_systematic_offset < -77.68 sec ruled out at XX sigma')
-    frac = 1- (0.000000439560* 0.015987957966*0.026633342800)
+    print('multiply 6b,18b, 46b: t_systematic_offset < -81.50 sec ruled out at XX sigma')
+    frac = 1- val_6b*val_46b*val_18b
+    print('product is {}'.format(val_6b*val_46b*val_18b))
     get_sigma_given_percent(frac)
