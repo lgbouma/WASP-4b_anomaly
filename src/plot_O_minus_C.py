898b952e16146294516894324bb4a02baf359687
lgbouma <bouma.luke@gmail.com>
2019-07-23 13:39:24 -0400

    O minus C: add onlytransits option
    lets you make an O-C diagram with only the transits, rather than
    transits + occultations.
    
    [TSC I prep]

--- src/plot_O_minus_C.py
+++ src/plot_O_minus_C.py
@@ -156,11 +156,158 @@ def plot_O_minus_C(
     xlim=None, ylim=None, ylim1=None,
     include_all_points=False,
     x_extra=None, y_extra=None, sigma_y_extra=None,
-    x_occ=None, y_occ=None, sigma_y_occ=None):
+    x_occ=None, y_occ=None, sigma_y_occ=None,
+    onlytransits=False):
 
     xfit = np.linspace(10*np.min(x), 10*np.max(x), 10000)
 
-    if (
+    if onlytransits:
+
+        fig, a0 = plt.subplots(nrows=1, ncols=1, figsize=(4*1.3,3*1.3))
+
+        print('USING HACK TO NOT PLOT TESS DATA POINTS')
+        istess = x > 1600
+
+        # transit axis
+        for e, tm, err in zip(x[~istess],y[~istess],sigma_y[~istess]):
+            a0.errorbar(e,
+                        nparr(tm-linear_fit(theta_linear, e)),
+                        err,
+                        fmt='.k', ecolor='black', zorder=10, mew=0,
+                        ms=7,
+                        elinewidth=1,
+                        alpha= 1-(err/np.max(sigma_y))**(1/2) + 0.1
+                       )
+
+        # for legend
+        a0.errorbar(9001, 9001, np.mean(err),
+                    fmt='.k', ecolor='black', zorder=9, alpha=1, mew=1, ms=3,
+                    elinewidth=1, label='pre-TESS')
+
+
+        # bin TESS points
+        is_tess = (refs=='me')
+        tess_x = x[is_tess]
+        tess_y = y[is_tess]
+        tess_sigma_y = sigma_y[is_tess]
+
+        bin_tess_y = np.average(nparr(tess_y-linear_fit(theta_linear, tess_x)),
+                                weights=1/tess_sigma_y**2)
+        bin_tess_sigma_y = np.mean(tess_sigma_y)/len(tess_y)**(1/2)
+        bin_tess_x = np.median(tess_x)
+
+        print('\n----- error on binned tess measurement -----\n')
+        print('{:.2f} seconds'.format(bin_tess_sigma_y*60))
+
+        #a0.plot(bin_tess_x, bin_tess_y, alpha=1, mew=0.5,
+        #        zorder=42, label='binned TESS', markerfacecolor='yellow',
+        #        markersize=9, marker='*', color='black', lw=0)
+        a0.errorbar(bin_tess_x, bin_tess_y, bin_tess_sigma_y,
+                    alpha=1, zorder=11, label='binned TESS',
+                    fmt='s', mfc='firebrick', elinewidth=1,
+                    ms=3,
+                    mec='firebrick',mew=1,
+                    ecolor='firebrick')
+
+
+        if include_all_points:
+            a0.errorbar(x_extra,
+                        nparr(y_extra-linear_fit(theta_linear, x_extra)),
+                        sigma_y_extra,
+                        fmt='.', color='g', ecolor='g', zorder=1,
+                        alpha=1, mew=1, elinewidth=1)
+            a0.text(0.96,0.02,
+                    'Bixel+ 2018 IMACS\n'+
+                    '(overplot; not included in fits)',
+                   transform=a0.transAxes, color='g', fontsize='x-small',
+                    va='bottom', ha='right')
+            #a0.text(0.96,0.02,
+            #        'select ETD times, & epoch from W+08\n'+
+            #        '(overplot; not included in fits)',
+            #       transform=a0.transAxes, color='g', fontsize='x-small',
+            #        va='bottom', ha='right')
+
+            # binned ETD point
+            etd_bin = (x_extra > 0)
+            etd_x = x_extra[etd_bin]
+            etd_y = y_extra[etd_bin]
+            etd_sigma_y = sigma_y_extra[etd_bin]
+
+            bin_etd_y = np.average(
+                nparr(etd_y-linear_fit(theta_linear, etd_x)),
+                weights = 1/etd_sigma_y**2
+            )
+            bin_etd_sigma_y = np.std(etd_sigma_y)
+            bin_etd_x = np.median(etd_x)
+
+            # NOTE: sometimes might want to show!
+            #a0.errorbar(bin_etd_x,
+            #            bin_etd_y,
+            #            bin_etd_sigma_y,
+            #            fmt='.', color='red', ecolor='red',
+            #            alpha=1, mew=1, elinewidth=1, zorder=8,
+            #            label='binned ETD time')
+
+        a0.plot(xfit,
+                quadratic_fit(theta_quadratic, xfit)
+                    - linear_fit(theta_linear, xfit),
+                zorder=-1)
+        a0.plot(xfit,
+                precession_fit(theta_prec, xfit)
+                    - linear_fit(theta_linear, xfit),
+                zorder=-2)
+        a0.plot(xfit,
+                linear_fit(theta_linear, xfit)
+                    - linear_fit(theta_linear, xfit),
+                zorder=-3, color='gray')
+
+        # now move on to the occultation axis!
+        #a0.text(0.98,0.95, 'Transits', transform=a0.transAxes, color='k',
+        #        fontsize='medium', va='top', ha='right')
+
+        # add "time" axis on top
+        # make twin axis to show year on top
+        period = 1.338231466*units.day
+        t0 = 2455804.515752*units.day
+        transittimes = x*period + t0
+        times = Time(transittimes, format='jd', scale='tdb')
+        a_top = a0.twiny()
+        a_top.scatter(times.decimalyear, np.zeros_like(times), s=0)
+        a_top.set_xlabel('Year', fontsize='large')
+
+        # hidden point for a1 legend
+        #a1.plot(1500, 3, alpha=1, mew=0.5,
+        #        zorder=-3, label='binned TESS time', markerfacecolor='yellow',
+        #        markersize=9, marker='*', color='black', lw=0)
+
+        if not include_all_points:
+            a0.legend(loc=(0.5, 0.02), fontsize='x-small', framealpha=1)
+        else:
+            a0.legend(loc=(0.2, 0.02), fontsize='x-small', framealpha=1)
+
+        a0.get_yaxis().set_tick_params(which='both', direction='in')
+        a0.get_xaxis().set_tick_params(which='both', direction='in')
+        a0.set_xlim(xlim)
+        a_top.get_yaxis().set_tick_params(which='both', direction='in')
+        a_top.get_xaxis().set_tick_params(which='both', direction='in')
+        a0.set_ylim((-1.6, 1.6))
+
+        fig.text(0.5,0, xlabel, ha='center', fontsize='large')
+        fig.text(-0.02,0.5, 'Transit obs. - calc. [minutes]', va='center', rotation=90, fontsize='large')
+
+        fig.tight_layout(h_pad=0, w_pad=0)
+        savpath = savpath.replace('.png','_onlytransits.png')
+        fig.savefig(savpath, bbox_inches='tight', dpi=400)
+        print('made {:s}'.format(savpath))
+        savpath = savpath.replace('.png','_onlytransits.pdf')
+        fig.savefig(savpath, bbox_inches='tight')
+        print('made {:s}'.format(savpath))
+
+
+
+    elif (
+        not onlytransits
+        and
         isinstance(x_occ,np.ndarray)
         and
         isinstance(y_occ,np.ndarray)
@@ -441,15 +588,15 @@ def main(plname, xlim=None, ylim=None, include_all_points=False, ylim1=None):
 
     basedir = '/home/luke/Dropbox/proj/tessorbitaldecay/'
     transitpath = (
-        basedir+'data/{:s}_literature_and_TESS_times_O-C_vs_epoch_selected.csv'
+        basedir+'data/literature_plus_TESS_times/{:s}_literature_and_TESS_times_O-C_vs_epoch_selected.csv'
         .format(plname)
     )
     occpath = (
-        basedir+'data/{:s}_occultation_times_selected.csv'
+        basedir+'data/literature_plus_TESS_times/{:s}_occultation_times_selected.csv'
         .format(plname)
     )
 
-    pkldir = basedir+'results/model_comparison/'+plname+'/'
+    pkldir = basedir+'results/model_comparison/'+plname+'_arxiv_submitted/'
 
     print('getting data from {:s}'.format(transitpath))
     x, y, sigma_y, data, tcol, refs = get_data(datacsv=transitpath)
@@ -520,6 +667,7 @@ def main(plname, xlim=None, ylim=None, include_all_points=False, ylim1=None):
     else:
         savpath = pkldir + 'O_minus_C_includeallpoints.png'
 
+    onlytransits = True
     plot_O_minus_C(
         x, y, sigma_y,
         best_theta_linear, best_theta_quadratic, best_theta_prec,
@@ -530,18 +678,21 @@ def main(plname, xlim=None, ylim=None, include_all_points=False, ylim1=None):
         xlim=xlim, ylim=ylim,
         include_all_points=include_all_points,
         x_extra=x_extra, y_extra=y_extra, sigma_y_extra=sigma_y_extra,
-        x_occ=x_occ, y_occ=y_occ, sigma_y_occ=sigma_y_occ, ylim1=ylim1)
+        x_occ=x_occ, y_occ=y_occ, sigma_y_occ=sigma_y_occ, ylim1=ylim1,
+        onlytransits=onlytransits)
 
-    if not include_all_points:
+    if not include_all_points and not onlytransits:
         copyfile(savpath, '../paper/f4.png')
         print('saved ../paper/f4.png')
         copyfile(savpath.replace('.png','.pdf'), '../paper/f4.pdf')
         print('saved ../paper/f4.pdf')
-    else:
+    elif include_all_points and not onlytransits:
         copyfile(savpath, '../paper/f9.png')
         print('saved ../paper/f9.png')
         copyfile(savpath.replace('.png','.pdf'), '../paper/f9.pdf')
         print('saved ../paper/f9.pdf')
+    elif onlytransits:
+        print('made the O-C plot with only transits')
 
 
 if __name__=="__main__":
