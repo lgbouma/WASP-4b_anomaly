91595d4b1282f54ccf2638fa3a4cf1f74e2063f4
lgbouma <bouma.luke@gmail.com>
2020-01-23 18:11:50 -0500

    wrote some words

---
+++ paper/collect.sh
@@ -0,0 +1,5 @@
+cp ../results/O_minus_C_includeallpoints_onlytransits.pdf f1.pdf
+cp ../results/20190911_rv_fit.pdf f2.pdf
+cp ../data/WASP4_zorro_speckle/WASP-4_20190928_832_img.png f3a.png
+cp ../data/WASP4_zorro_speckle/WASP-4_20190928_832.pdf f3b.pdf
+cp ../results/mass_semimaj_constraints.pdf f4.pdf
