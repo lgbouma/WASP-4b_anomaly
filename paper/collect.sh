77c42c8ec490b9a9183cfbbece4392b9b006f43c
lgbouma <bouma.luke@gmail.com>
2020-01-24 09:42:44 -0500

    update paper

--- paper/collect.sh
+++ paper/collect.sh
@@ -3,3 +3,4 @@ cp ../results/20190911_rv_fit.pdf f2.pdf
 cp ../data/WASP4_zorro_speckle/WASP-4_20190928_832_img.png f3a.png
 cp ../data/WASP4_zorro_speckle/WASP-4_20190928_832.pdf f3b.pdf
 cp ../results/mass_semimaj_constraints.pdf f4.pdf
+cp ../results/k14_pdot.pdf f5.pdf
