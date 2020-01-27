746f3bb6000dd8e131ccdc4f6c841969e4fee59a
lgbouma <bouma.luke@gmail.com>
2020-01-27 16:18:45 -0500

    update paper

--- paper/collect.sh
+++ paper/collect.sh
@@ -1,4 +1,4 @@
-cp ../results/O_minus_C_includeallpoints_onlytransits.pdf f1.pdf
+cp ../results/O_minus_C_20200127_onlytransits.pdf f1.pdf
 cp ../results/20190911_rv_fit.pdf f2.pdf
 cp ../data/WASP4_zorro_speckle/WASP-4_20190928_832_img.png f3a.png
 cp ../data/WASP4_zorro_speckle/WASP-4_20190928_832.pdf f3b.pdf
