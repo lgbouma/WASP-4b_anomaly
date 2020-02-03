c73636e7a57c3cd981d298d1beab6a7c2c4b94ff
lgbouma <bouma.luke@gmail.com>
2020-02-03 17:18:31 -0500

    compile paper

--- paper/collect.sh
+++ paper/collect.sh
@@ -1,5 +1,5 @@
 cp ../results/O_minus_C_20200127_onlytransits.pdf f1.pdf
-cp ../results/20190911_rv_fit.pdf f2.pdf
+cp ../results/rv_fitting/LGB_20190911_fix_gammaddot_paperII/WASP4_rv_multipanel.pdf f2.pdf
 cp ../results/zorro_speckle.pdf f3.pdf
 cp ../results/mass_semimaj_constraints_with_contrast_discretecolor.pdf f4.pdf
 cp ../results/k14_pdot.pdf f5.pdf
